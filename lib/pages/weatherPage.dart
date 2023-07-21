dcf859cbcd09db6fcece17392b398b4efa2e121f
Grundig <sevgitr_@hotmail.com>
2023-07-21 19:44:12 +0300

    wheather page

---
+++ lib/pages/weatherPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _weatherPage();
+}
+
+class _weatherPage extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
