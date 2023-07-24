20d0a87c8c9dce9905668140dca3e2cbc0855f9f
sevgitr <sevgitr_@hotmail.com>
2023-07-25 02:41:15 +0300

    connect class ( pizza )

---
+++ lib/controllers/connect_class.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+
+void main() async {
+  var client = http.Client();
+  var request = http.Request(
+    'GET',
+    Uri.parse('https://pizza-and-desserts.p.rapidapi.com/pizzas'),
+  );
+  
+  request.headers.addAll({
+    "X-RapidAPI-Key": "d3875fdd0emshc5ad7f5a5426f1ap18e44djsn795b094265e8",
+    "X-RapidAPI-Host": "pizza-and-desserts.p.rapidapi.com"
+  });
+
+   var response = await client.send(request);
+   if (response.statusCode == 200) {
+      var body = await response.stream.bytesToString();
+      print(body);
+   } else {
+      print('Request failed with status: ${response.statusCode}.');
+   }
+
+   client.close();
+}
